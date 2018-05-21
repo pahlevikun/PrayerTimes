87887889526586b4623b01c5940901a822f70463
farhan <pahlevi.kun@gmail.com>
2018-05-22 01:01:33 +0700

    Android: Add Model - 22/05/18 01:01

---
+++ Android/PrayerTimes/app/src/main/java/com/deggan/prayertimes/Time.kt
@@ -0,0 +1,21 @@
+package com.deggan.prayertimes
+
+/**
+ * Created by farhan on 5/22/18.
+ */
+data class Time(fajr: String,
+                sunrise: String,
+                dhuhr: String,
+                asr: String,
+                sunset: String,
+                maghrib: String,
+                isha: String,
+                imsak: String,
+                midnight: String,
+                gregorian: String,
+                hijri: String,
+                timeStamp: String,
+                latitude: Double,
+                longitude: Double,
+                timeZone: String,
+                method: String)
